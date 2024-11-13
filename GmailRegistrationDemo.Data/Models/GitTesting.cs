8c07e7daddeb7eeb28a746600ec8308aeb130ac3
rimsha-khalid <rimsha.khalid@systemsltd.com>
2024-11-13 19:31:09 +0500

    Update in Project

---
+++ GmailRegistrationDemo.Data/Models/GitTesting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GmailRegistrationDemo.Data.Models
+{
+    internal class GitTesting
+    {
+    }
+}
